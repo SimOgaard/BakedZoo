c0ea8ac95cd15001aa8a076327e0dd9fcab4b01e
abbsimoga <Simon.OgaardJozic@abbindustrigymnasium.se>
2021-05-29 16:04:50 +0200

    Update Donations.vue

--- BakedZoo/src/pages/Donations.vue
+++ BakedZoo/src/pages/Donations.vue
@@ -1,6 +1,7 @@
 <template>
-  <q-page class="flex-center">
-    <h4 class="q-mt-sm q-mb-none text-primary text-weight-bold text-center">Swisha 0739055618</h4>
+  <q-page class="column flex-center items-center q-pb-xl">
+    <h4 class="row q-mt-sm q-mb-none text-primary text-weight-bold text-center">Swisha</h4>
+    <h4 class="row q-mt-sm q-mb-none text-primary text-weight-normal text-center">0739055618</h4>
   </q-page>
 </template>
 
